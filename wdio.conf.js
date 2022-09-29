93b03a4fcbd7996b1358220b44592a2fd8f3070e
Masatoshi Hashimoto <hashimoto.ma-06@fujitsu.com>
2022-09-29 05:12:20 +0000

    Edge対応

--- wdio.conf.js
+++ wdio.conf.js
@@ -77,7 +77,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'localhost',
+    baseUrl: 'https://4339639atrial.launchpad.cfapps.us10.hana.ondemand.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
--- webapp/test/specs/tester.js
+++ webapp/test/specs/tester.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('Google', function() {
   it('should display the title', function() {
-      browser.url('https://4339639atrial.launchpad.cfapps.us10.hana.ondemand.com/cd3c862d-0a08-4f9a-96ca-cf91c8e3f50e.test.test-0.0.1/index.html');
+      browser.url('/cd3c862d-0a08-4f9a-96ca-cf91c8e3f50e.test.test-0.0.1/index.html');
       assert(browser.getTitle() === 'CID Test');
   });  
 });
