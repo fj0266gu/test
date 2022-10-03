20c5b4f6e641025c3a3f4e3f2dfd4de0e87612d6
Masatoshi Hashimoto <hashimoto.ma-06@fujitsu.com>
2022-10-03 09:15:08 +0000

    オプション追加

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,6 +57,9 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', 'window-size=1024,768','--no-sandbox"']
+        },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
