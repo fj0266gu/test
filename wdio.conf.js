bae99cb3cbde700fc39868537126ede50808081f
Masatoshi Hashimoto <hashimoto.ma-06@fujitsu.com>
2022-10-03 09:07:34 +0000

    オプション削除

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,9 +57,6 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        chromeOptions: {
-            args: ['--no-sandbox', '--disable-dev-shm-usage']
-        },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
