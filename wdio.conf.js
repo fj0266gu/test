34b36ac58d02eb74248cc9b92fdfdcdc5e4a2ec6
fj0266gu <107452308+fj0266gu@users.noreply.github.com>
2023-01-24 18:10:49 +0900

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -97,7 +97,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://4339639atrial.launchpad.cfapps.us10.hana.ondemand.com',
+    baseUrl: 'https://437db673trial.launchpad.cfapps.us10.hana.ondemand.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
