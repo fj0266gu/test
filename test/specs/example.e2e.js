13b2adcee91807f0dca1666d18aa8f9aa98ef5d7
fj0266gu <107452308+fj0266gu@users.noreply.github.com>
2023-01-24 18:09:59 +0900

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -15,7 +15,7 @@ const assert = require('assert');
 
 describe('CID Test', function() {
    it('should display the title', async () => {
-      await browser.url('/4d8324b4-f741-4854-912b-a9947e2fcbf3.test.test-0.0.1/index.html');
+      await browser.url('/b699b295-2c1e-42df-9c4b-1b26519872c8.test.test-0.0.1/index.html');
       var title = await browser.getTitle();
       console.log("title=" + title);
       assert(title === 'Log On');
